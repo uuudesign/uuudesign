9e2655074c0b37251a47ad4052747c05bd58152b
Andrey <alexandrovit@gmail.com>
2012-11-29 10:01:41 -0500

    [0h 10m] init2

---
+++ search.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Privacy Policy</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
+	<link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/skeleton.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/superfish.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<link rel="stylesheet" href="css/forms.css" type="text/css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<script src="js/jquery-1.7.1.min.js"></script>
+	<script src="js/script.js"></script>
+	<script src="js/jquery.hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+	<script src="js/jquery.responsivemenu.js"></script>
+	<script src="search/search.js"></script>
+	<script src="js/jquery.ui.totop.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!--[if lt IE 8]>
+		<div style='clear: both; text-align:center; position: relative;'>
+			<a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+				<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today.">
+			</a>
+		</div>
+	<![endif]-->
+	<!--[if lt IE 9]>
+		<script src="js/html5.js"></script>
+		<link rel="stylesheet" href="css/ie.css"> 
+	<![endif]-->
+	<!--[if IE 9]>
+		<style>
+			#search-form input{padding:8px 45px 8px 10px;}
+		</style>
+	<![endif]-->
+</head>
+<body id="page7">
+	<!--======================== header ============================-->
+	<header>
+		<div class="bg1">
+			<div class="bg2">
+				<div class="container_12">
+					<div class="grid_12">
+						<div class="wrapper p5">
+							<!--======================== logo ============================-->
+							<h1><a href="index.html">fortiz management consulting</a></h1>
+							<!--======================= search ===========================-->
+							<form id="search-form" action="search.php" method="GET" accept-charset="utf-8">
+								<fieldset>
+									<label class="keywords">
+										<input type="text" name="s">
+									</label>
+									<a onClick="document.getElementById('search-form').submit()"></a>
+								</fieldset>
+							</form>
+						</div>
+						<!--======================== menu ============================-->
+						<nav>
+							<ul class="menu responsive-menu">
+								<li class="first-item"><a href="index.html">Home</a></li>
+								<li><a href="index-1.html">About</a>
+									<ul>
+										<li><a href="#">History</a></li>
+										<li><a href="#">News</a>
+											<ul>
+												<li><a href="#">Latest</a></li>
+												<li class="last-item"><a href="#">Archive</a></li>
+											</ul>
+										</li>
+										<li class="last-item"><a href="#">Testimonials</a></li>
+									</ul>
+								</li>
+								<li><a href="index-2.html">Projects</a></li>
+								<li><a href="index-3.html">Services</a></li>
+								<li><a href="index-4.html">Blog</a></li>
+								<li class="last-item"><a href="index-5.html">Contacts</a></li>
+							</ul>
+							<div class="clear"></div>
+						</nav>
+					</div>
+					<div class="clear"></div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!--======================== content ===========================-->
+	<section id="content">
+		<div class="container_12">
+			<div class="wrapper">
+				<div class="grid_12">
+					<h3 class="p3">Search result:</h3>
+					<div id="search-results"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--======================== footer ============================-->
+	<footer>
+		<div class="container_12">
+			<div class="wrapper">
+				<div class="grid_12">
+					<div class="wrapper">
+						<div class="indent-bottom">
+							<div class="copy"><a href="index.html" class="footer-logo"></a> © 2012 | <a href="index-6.html">Privacy Policy</a></div>
+							<ul class="footer-menu hide-ml">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="index-1.html">About</a></li>
+								<li><a href="index-2.html">Projects</a></li>
+								<li><a href="index-3.html">Services</a></li>
+								<li><a href="index-4.html">Blog</a></li>
+								<li class="last-item"><a href="index-5.html">Contacts</a></li>
+							</ul>
+							<div class="clear hide-ml"></div>
+						</div>
+						<ul class="social-icons">
+							<li><a href="#" class="icon-1"></a></li>
+							<li><a href="#" class="icon-2"></a></li>
+							<li><a href="#" class="icon-3"></a></li>
+							<li><a href="#" class="icon-4"></a></li>
+							<li class="last-item"><a href="#" class="icon-5"></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
